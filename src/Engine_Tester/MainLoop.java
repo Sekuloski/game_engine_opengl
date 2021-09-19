e70d387b367780ddd8642f3ff5b0fdf6eb4cdda3
Bojan <sekuloski.bojan18@gmail.com>
2021-09-19 14:01:38 +0200

    Changes to VAOs. Now I am using indices instead of duplicate vertices.

--- src/Engine_Tester/MainLoop.java
+++ src/Engine_Tester/MainLoop.java
@@ -17,16 +17,21 @@ public class MainLoop
         Loader loader = new Loader();
         Renderer renderer = new Renderer();
 
-        float[] vertices = {
+        float[] vertices =
+        {
                 -0.5f, 0.5f, 0f,
                 -0.5f, -0.5f, 0f,
                 0.5f, -0.5f, 0f,
-                0.5f, -0.5f, 0f,
-                0.5f, 0.5f, 0f,
-                -0.5f, 0.5f, 0f
+                0.5f, 0.5f, 0f
         };
 
-        RawModel model = loader.loadToVAO(vertices);
+        int[] indices =
+        {
+            0, 1, 3,
+            3, 1, 2
+        };
+
+        RawModel model = loader.loadToVAO(vertices, indices);
 
         while (!Display.isCloseRequested())
         {
--- src/Render_Engine/Loader.java
+++ src/Render_Engine/Loader.java
@@ -7,21 +7,23 @@ import org.lwjgl.opengl.GL20;
 import org.lwjgl.opengl.GL30;
 
 import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Loader
 {
 
-    private final List<Integer> vaos = new ArrayList<Integer>();
-    private final List<Integer> vbos = new ArrayList<Integer>();
+    private final List<Integer> vaos = new ArrayList<>();
+    private final List<Integer> vbos = new ArrayList<>();
 
-    public RawModel loadToVAO(float[] positions)
+    public RawModel loadToVAO(float[] positions, int[] indices)
     {
         int vaoID = createVAO();
+        bindIndicesBuffer(indices);
         storeDataInAttributeList(0, positions);
         unbindVAO();
-        return new RawModel(vaoID, positions.length/3);
+        return new RawModel(vaoID, indices.length);
     }
 
     private int createVAO()
@@ -51,6 +53,23 @@ public class Loader
     private FloatBuffer storeDataInFloatBuffer(float[] data)
     {
         FloatBuffer buffer = BufferUtils.createFloatBuffer(data.length);
+        buffer.put(data);
+        buffer.flip();
+        return buffer;
+    }
+
+    private void bindIndicesBuffer(int[] indices)
+    {
+        int vboID = GL15.glGenBuffers();
+        vbos.add(vboID);
+        GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, vboID);
+        IntBuffer buffer = storeDataInIntBuffer(indices);
+        GL15.glBufferData(GL15.GL_ELEMENT_ARRAY_BUFFER, buffer, GL15.GL_STATIC_DRAW);
+    }
+
+    private IntBuffer storeDataInIntBuffer(int[] data)
+    {
+        IntBuffer buffer = BufferUtils.createIntBuffer(data.length);
         buffer.put(data);
         buffer.flip();
         return buffer;
