495ab8c822b4befb34263867a8c86a319aa52038
Bojan <sekuloski.bojan18@gmail.com>
2021-10-02 13:04:49 +0200

    Added detachable camera.

--- src/Entities/Camera.java
+++ src/Entities/Camera.java
@@ -1,18 +1,36 @@
 package Entities;
 
+import Engine_Tester.MainLoop;
+import Render_Engine.DisplayManager;
+import org.lwjgl.input.Keyboard;
 import org.lwjgl.input.Mouse;
 import org.lwjgl.util.vector.Vector3f;
 
 public class Camera
 {
+    private static final float RUN_SPEED = 100;
+
+    private float currentSpeed = 0;
+    private float currentTurnSpeed = 0;
+    private float currentFlySpeed = 0;
 
     private final Vector3f position = new Vector3f(100, 35, 50);
     private float pitch = 10;
     private float yaw = 0;
     private float roll;
     private float distanceFromPlayer = 0;
-    private float angleAroundPlayer = 0;
 
+    private float lastPitch = pitch;
+    private float lastYaw = yaw;
+    private float lastRoll = roll;
+
+    //private float angleAroundPlayer = 0;
+
+    public static boolean detached = false;
+    private boolean flag = false;
+    private boolean flag2 = true;
+    private boolean flying = false;
+    private boolean falling = false;
 
     private Player player;
 
@@ -24,11 +42,167 @@ public class Camera
     public void move()
     {
        // calculateAngle();
-        calculatePitch();
-        float horizontal = calculateHorDistance();
-        float vertical = calculateVerDistance();
-        calculatePosition(horizontal, vertical);
+        checkInputs();
+        if(detached)
+        {
+            if(flag2)
+            {
+                lastPitch = pitch;
+                lastYaw = yaw;
+                lastRoll = roll;
+                flag2 = false;
+            }
+            changeRotation(0, MainLoop.DX,-Mouse.getDY() * 0.1f);
 
+            float distance = currentSpeed * DisplayManager.getDelta();
+            float dx = (float) (distance * Math.sin(Math.toRadians(player.getRy())));
+            float dz = (float) (distance * Math.cos(Math.toRadians(player.getRy())));
+            changePosition(dx, 0, dz);
+
+            distance = currentFlySpeed * DisplayManager.getDelta();
+            changePosition(0, distance, 0);
+
+            distance = currentTurnSpeed * DisplayManager.getDelta();
+            dx = (float) (distance * Math.sin(Math.toRadians(player.getRy() + 90)));
+            dz = (float) (distance * Math.cos(Math.toRadians(player.getRy() + 90)));
+            changePosition(dx, 0, dz);
+        }
+        else
+        {
+            if(!flag2)
+            {
+                pitch = lastPitch;
+                yaw = lastYaw;
+                roll = lastRoll;
+                flag2 = true;
+            }
+
+            calculatePitch();
+            float horizontal = calculateHorDistance();
+            float vertical = calculateVerDistance();
+            calculatePosition(horizontal, vertical);
+        }
+
+    }
+
+    public void changePosition(float dx, float dy, float dz)
+    {
+        this.position.x += dx;
+        this.position.y += dy;
+        this.position.z += dz;
+    }
+
+    public void changeRotation(float dx, float dy, float dz)
+    {
+        this.roll += dx;
+        this.yaw += dy;
+        this.pitch += dz;
+    }
+
+    private void checkInputs()
+    {
+        if (Keyboard.isKeyDown(Keyboard.KEY_W))
+        {
+            this.currentSpeed = RUN_SPEED;
+        } else if (Keyboard.isKeyDown(Keyboard.KEY_S))
+        {
+            this.currentSpeed = -RUN_SPEED;
+        } else
+        {
+            this.currentSpeed = 0;
+        }
+
+        if (Keyboard.isKeyDown(Keyboard.KEY_D))
+        {
+            this.currentTurnSpeed = -RUN_SPEED;
+        } else if (Keyboard.isKeyDown(Keyboard.KEY_A))
+        {
+            this.currentTurnSpeed = RUN_SPEED;
+        } else
+        {
+            this.currentTurnSpeed = 0;
+        }
+
+        if (!falling && Keyboard.isKeyDown(Keyboard.KEY_SPACE))
+        {
+            flying = true;
+            this.currentFlySpeed = RUN_SPEED;
+        }
+        else if(!falling)
+        {
+            flying = false;
+            this.currentFlySpeed = 0;
+        }
+
+        if (!flying && Keyboard.isKeyDown(Keyboard.KEY_LSHIFT))
+        {
+            falling = true;
+            this.currentFlySpeed = -RUN_SPEED;
+        }
+        else if(!flying)
+        {
+            falling = false;
+            this.currentFlySpeed = 0;
+        }
+
+        if(Keyboard.isKeyDown(Keyboard.KEY_O))
+        {
+            if(flag)
+            {
+                detached = !detached;
+                System.out.println(detached);
+            }
+            flag = false;
+        }
+        else
+        {
+            flag = true;
+        }
+
+    }
+
+    private void calculatePosition(float horizontal, float vertical)
+    {
+        float theta = player.getRy()/* +  angleAroundPlayer */;
+        float offsetX = horizontal * (float) Math.sin(Math.toRadians(theta));
+        float offsetZ = horizontal * (float) Math.cos(Math.toRadians(theta));
+        position.x = player.getPosition().x - offsetX;
+        position.z = player.getPosition().z - offsetZ;
+        position.y = player.getPosition().y + vertical + 10;
+        this.yaw = 180 - (player.getRy()/* +  angleAroundPlayer */);
+    }
+
+    private float calculateHorDistance()
+    {
+        return distanceFromPlayer * (float) Math.cos(Math.toRadians(pitch));
+    }
+
+    private float calculateVerDistance()
+    {
+        return distanceFromPlayer * (float) Math.sin(Math.toRadians(pitch));
+    }
+
+    private void calculateZoom()
+    {
+        float zoomLevel = Mouse.getDWheel() * 0.1f;
+        distanceFromPlayer -= zoomLevel;
+    }
+
+    private void calculatePitch()
+    {
+        if(!Player.escPressed)
+        {
+            float pitchChange = Mouse.getDY() * 0.1f;
+            pitch -= pitchChange;
+            if(pitch < -90)
+            {
+                pitch = -90;
+            }
+            if(pitch > 90)
+            {
+                pitch = 90;
+            }
+        }
     }
 
     public Vector3f getPosition()
@@ -49,50 +223,6 @@ public class Camera
     public float getRoll()
     {
         return roll;
-    }
-
-    private void calculatePosition(float horizontal, float vertical)
-    {
-        float theta = player.getRy() + angleAroundPlayer;
-        float offsetX = horizontal * (float) Math.sin(Math.toRadians(theta));
-        float offsetZ = horizontal * (float) Math.cos(Math.toRadians(theta));
-        position.x = player.getPosition().x - offsetX;
-        position.z = player.getPosition().z - offsetZ;
-        position.y = player.getPosition().y + vertical + 10;
-        this.yaw = 180 - (player.getRy() + angleAroundPlayer);
-    }
-
-    private float calculateHorDistance()
-    {
-        return distanceFromPlayer * (float) Math.cos(Math.toRadians(pitch));
-    }
-
-    private float calculateVerDistance()
-    {
-        return distanceFromPlayer * (float) Math.sin(Math.toRadians(pitch));
-    }
-
-    private void calculateZoom()
-    {
-        //float zoomLevel = Mouse.getDWheel() * 0.1f;
-        //distanceFromPlayer -= zoomLevel;
-    }
-
-    private void calculatePitch()
-    {
-        if(!Player.escPressed)
-        {
-            float pitchChange = Mouse.getDY() * 0.1f;
-            pitch -= pitchChange;
-            if(pitch < -90)
-            {
-                pitch = -90;
-            }
-            if(pitch > 90)
-            {
-                pitch = 90;
-            }
-        }
     }
 
 //    private void calculateAngle()
--- src/Entities/Player.java
+++ src/Entities/Player.java
@@ -1,16 +1,16 @@
 package Entities;
 
+import Engine_Tester.MainLoop;
 import Models.TexturedModel;
 import Render_Engine.DisplayManager;
 import Terrains.Terrain;
 import org.lwjgl.input.Keyboard;
-import org.lwjgl.input.Mouse;
 import org.lwjgl.util.vector.Vector3f;
 
 public class Player extends Entity
 {
 
-    private static final float RUN_SPEED = 50;
+    private static float RUN_SPEED = 50;
     private static final float TURN_SPEED = 160;
     private static final float GRAVITY = -50;
     private static final float JUMP = 30;
@@ -20,42 +20,75 @@ public class Player extends Entity
     private float currentTurnSpeed = 0;
     private float upwardsSpeed = 0;
     private boolean grounded = true;
+    private boolean crouching = false;
+    private boolean sprinting = false;
+    private float lastRX;
+    private float lastRY;
+    private float lastRZ;
 
     public static boolean escPressed = false;
+    public static boolean flag = true;
 
     public Player(TexturedModel model, Vector3f position, float rx, float ry, float rz, float scale)
     {
         super(model, position, rx, ry, rz, scale);
+        lastRX = rx;
+        lastRY = ry;
+        lastRZ = rz;
     }
 
     public void move(Terrain terrain)
     {
-        checkInputs();
-        if(!escPressed)
+        if(!Camera.detached)
         {
-            super.changeRotation(0, -Mouse.getDX() * 0.3f, 0);
+            if(!flag)
+            {
+                setRx(lastRX);
+                setRy(lastRY);
+                setRz(lastRZ);
+                flag = true;
+            }
+            checkInputs();
+            if(!escPressed)
+            {
+                super.changeRotation(0, -MainLoop.DX, 0);
+            }
+
+            float distance = currentSpeed * DisplayManager.getDelta();
+            float dx = (float) (distance * Math.sin(Math.toRadians(super.getRy())));
+            float dz = (float) (distance * Math.cos(Math.toRadians(super.getRy())));
+            super.changePosition(dx, 0, dz);
+
+            distance = currentTurnSpeed * DisplayManager.getDelta();
+            dx = (float) (distance * Math.sin(Math.toRadians(super.getRy() + 90)));
+            dz = (float) (distance * Math.cos(Math.toRadians(super.getRy() + 90)));
+            super.changePosition(dx, 0, dz);
+
+            upwardsSpeed += GRAVITY * DisplayManager.getDelta();
+            super.changePosition(0, upwardsSpeed * DisplayManager.getDelta(), 0);
+
+            float height = terrain.getHeightOfTerrain(super.getPosition().x, super.getPosition().z);
+
+            if(super.getPosition().y <= height)
+            {
+                upwardsSpeed = 0;
+                grounded = true;
+                super.getPosition().y = height;
+            }
         }
-
-        float distance = currentSpeed * DisplayManager.getDelta();
-        float dx = (float) (distance * Math.sin(Math.toRadians(super.getRy())));
-        float dz = (float) (distance * Math.cos(Math.toRadians(super.getRy())));
-        super.changePosition(dx, 0, dz);
-
-        distance = currentTurnSpeed * DisplayManager.getDelta();
-        dz = (float) (distance * Math.sin(Math.toRadians(super.getRy())));
-        dx = (float) (distance * Math.cos(Math.toRadians(super.getRy())));
-        super.changePosition(dx, 0, dz);
-
-        upwardsSpeed += GRAVITY * DisplayManager.getDelta();
-        super.changePosition(0, upwardsSpeed * DisplayManager.getDelta(), 0);
-
-        float height = terrain.getHeightOfTerrain(super.getPosition().x, super.getPosition().z);
-
-        if(super.getPosition().y <= height)
+        else
         {
-            upwardsSpeed = 0;
-            grounded = true;
-            super.getPosition().y = height;
+            if(flag)
+            {
+                lastRX = getRx();
+                lastRY = getRy();
+                lastRZ = getRz();
+                flag = false;
+            }
+            if(!escPressed)
+            {
+                super.changeRotation(0, -MainLoop.DX, 0);
+            }
         }
 
     }
@@ -82,11 +115,11 @@ public class Player extends Entity
 
         if(Keyboard.isKeyDown(Keyboard.KEY_D))
         {
-            this.currentTurnSpeed = RUN_SPEED;
+            this.currentTurnSpeed = -RUN_SPEED;
         }
         else if(Keyboard.isKeyDown(Keyboard.KEY_A))
         {
-            this.currentTurnSpeed = -RUN_SPEED;
+            this.currentTurnSpeed = RUN_SPEED;
         }
         else
         {
@@ -100,6 +133,31 @@ public class Player extends Entity
                 jump();
                 grounded = false;
             }
+        }
+
+        if(!crouching && Keyboard.isKeyDown(Keyboard.KEY_LSHIFT))
+        {
+            RUN_SPEED = 75;
+            sprinting = true;
+        }
+        else
+        {
+            sprinting = false;
+        }
+
+        if(!sprinting && Keyboard.isKeyDown(Keyboard.KEY_LCONTROL))
+        {
+            RUN_SPEED = 25;
+            crouching = true;
+        }
+        else
+        {
+            crouching = false;
+        }
+
+        if(!crouching && !sprinting)
+        {
+            RUN_SPEED = 50;
         }
 //        if(Keyboard.isKeyDown(Keyboard.KEY_LSHIFT))
 //        {
